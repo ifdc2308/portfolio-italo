abf22ede98a1e2de3aa30f25f198f43cc616a9ac
@ifdc2308 <italodorea123@gmail.com>
2024-04-05 19:49:10 -0300

    feat: header

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import { useState } from 'react'
-import './App.css'
-import SearchAppBar from './views/home'
+import { React, useState } from "react";
+import "./App.css";
+import SearchAppBar from "./views/home";
+import Container from "@mui/material/Container";
 
 function App() {
- 
   return (
     <>
-      <SearchAppBar/>
+      <Container fixed sx={{ width: "100vw" }}>
+        <SearchAppBar />
+      </Container>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
