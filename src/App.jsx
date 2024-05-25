eaa4de5266efc4e47d43fcaf8a917de93a7509e4
ifdc2308 <italodorea123@gmail.com>
2024-05-25 13:43:13 -0300

    feat: name-image

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import { HomePage } from './views/HomePage';
-
 function App() {
   return (
     <>
       <HomePage />
-    </>
+      </>
   );
 }
 
--- src/views/HomePage.jsx
+++ src/views/HomePage.jsx
@@ -1,4 +1,5 @@
-import { Container, Box, Grid, Typography as Text, Avatar, Divider } from '@mui/material';
+import { Container, Box, Grid, Typography as Text, Avatar, Button } from '@mui/material';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 
 export const HomePage = () => {
   return (
@@ -6,12 +7,16 @@ export const HomePage = () => {
       <Container maxWidth="lg" sx={styles.container}>
         <Grid container spacing={2}>
           <Grid item xs={12} sm={6}>
-          <Avatar alt="italo_dorea" src="../assets/profile_italo_dorea.jpg" />
-            <Text>Ítalo Dórea</Text>
-            <Text>Desenvolvedor Front-end</Text>
+            <Avatar alt="italo_dorea" src="../assets/profile_italo_dorea.jpg" />
+            <Text sx={styles.subTitle}>Ítalo Dórea</Text>
+            <Text sx={styles.title}>Desenvolvedor Front-end</Text>
+            <Text sx={styles.desc}>Olá descrição</Text>
+            <Button sx={{color: "#A1A1AA", border: "1px solid #A1A1AA"}} variant="outlined" startIcon={<WhatsAppIcon sx={{color: "#25D366"}} />}>
+              Entre em contato
+            </Button>
           </Grid>
           <Grid item xs={12} sm={6}>
-            <Text>Entrar em contato</Text>
+           <Text>Aqui vai ter uma imagem de background</Text>
           </Grid>
         </Grid>
       </Container>
@@ -25,5 +30,22 @@ const styles = {
   },
   title: {
     color: "#fff",
+    fontWeight: 700,
+    fontSize: 35,
+  },
+  subTitle: {
+    color: "#fff",
+    fontWeight: 500,
+    fontSize: 30,
+  },
+  text: {
+    color: "#fff",
+    fontWeight: 400,
+    fontSize: 22,
+  },
+  desc: {
+    color: "#A1A1AA",
+    fontWeight: 400,
+    fontSize: 20,
   },
 }
